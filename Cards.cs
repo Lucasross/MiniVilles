be4c0763a8ce7e902642882c088ce49360bec3c6
gyfukfyu <guill.lambert@orange.fr>
2020-10-28 12:20:15 +0100

    cards

--- Cards.cs
+++ Cards.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace MiniVilles
 {
-    class Cards
+    public abstract class Cards
     {
+        protected int activationValue;
+        protected string color;
+        protected int cardCost;
+        protected int givenCoin;
+
+        public Cards(int activationValue, string color, int cardCost, int givenCoin)
+        {
+            this.activationValue = activationValue;
+            this.color = color;
+            this.cardCost = cardCost;
+            this.givenCoin = givenCoin;
+        }
+
+        public abstract int CardEffect();
     }
 }
--- Player.cs
+++ Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiniVilles
 {
-    class Player
+    public class Player
     {
     }
 }
